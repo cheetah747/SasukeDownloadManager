3b5d6a026501d96918b0d131b7850b40f993a0e0
cheetah747 <cheetah747@qq.com>
2018-07-23 23:20:02 +0800

    TitleBarを完全消す！！！

--- app/src/main/java/com/sibyl/sasukedownloadmanager/activity/BaseActivity.kt
+++ app/src/main/java/com/sibyl/sasukedownloadmanager/activity/BaseActivity.kt
@@ -1,12 +1,12 @@
 package com.sibyl.sasukedownloadmanager.activity
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 
 /**
  * @author Sasuke on 2018/7/23.
  */
-open class BaseActivity: AppCompatActivity() {
+open class BaseActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
